800bb475f8677f921035f4b5e6c9fe3b938b5a1d
Anıl Selvi (ETIYA) <anil.selvi@etiya.com>
2020-02-02 02:16:56 +0300

    fix

--- inventory/settings.py
+++ inventory/settings.py
@@ -156,3 +156,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
