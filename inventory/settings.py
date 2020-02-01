a13b82071ce98bf5b802edc597241308ad8df325
Anıl Selvi (ETIYA) <anil.selvi@etiya.com>
2020-02-02 02:34:22 +0300

    asda

--- inventory/settings.py
+++ inventory/settings.py
@@ -160,6 +160,3 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'project_name/static')
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
